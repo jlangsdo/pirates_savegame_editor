1d546399452d69cda80272f25ab1c9ad0bff5bb2
Langsdorf <langsdorf@langsdorfs-macbook-air.local>
2019-03-29 17:37:20 -0400

    Initial Commit

---
+++ pirates_savegame_editor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pirates_savegame_editor
+//
+//  Created by Langsdorf on 3/29/19.
+//  Copyright © 2019 Langsdorf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
